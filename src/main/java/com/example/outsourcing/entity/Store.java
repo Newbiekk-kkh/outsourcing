004bb1785eb66ddd5cdc047c5bab2420da1d3a4b
newbiekk <newbiekk1126@gmail.com>
2024-12-03 14:27:38 +0900

    feat : entity 생성
    entity 생성 및 그에 맞는 enum 생성
    
    # Conflicts:
    #	src/main/java/com/example/outsourcing/entity/Member.java

--- src/main/java/com/example/outsourcing/entity/Store.java
+++ src/main/java/com/example/outsourcing/entity/Store.java
@@ -1,26 +1,18 @@
 package com.example.outsourcing.entity;
 
-import com.example.outsourcing.Store.dto.StoreRequestDto;
 import com.example.outsourcing.eunm.StoreStatus;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.sql.Time;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "store")
-public class Store extends BaseEntity {
+public class Store {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(nullable = false)
-    private String name;
 
     @Column(nullable = false)
     private long price;
@@ -29,41 +21,23 @@ public class Store extends BaseEntity {
     @Column(nullable = false)
     private StoreStatus status;
 
-//    @Column(nullable = false)
-    private LocalTime openTime;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIME)
+    private Time openTime;
 
-//    @Column(nullable = false)
-    private LocalTime closeTime;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIME)
+    private Time closeTime;
 
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @OneToMany(mappedBy = "store")
-    private List<Menu> menus = new ArrayList<>();
-
-
     public Store() {
     }
 
-    @Builder
-    public Store(String name, long price, LocalTime openTime, LocalTime closeTime, Member member) {
-        this.name = name;
+    public Store(long price, StoreStatus status) {
         this.price = price;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
-        this.status = StoreStatus.NORMAL;
-        this.member = member;
-    }
-
-    public void update(StoreRequestDto dto) {
-        this.name = dto.getName();
-        this.price = dto.getDefaultPrice();
-        this.openTime = dto.getOpenTIme();
-        this.closeTime = dto.getCloseTime();
-    }
-
-    public void delete(){
-        this.status = StoreStatus.DELETED;
+        this.status = status;
     }
 }
