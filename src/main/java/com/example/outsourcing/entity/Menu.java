f0aa6c2dd564e1ef13d767d3065b25062ab4763d
Mini9709 <tjdals9709@naver.com>
2024-12-04 16:52:58 +0900

    feat : MenuStatus 추가

--- src/main/java/com/example/outsourcing/entity/Menu.java
+++ src/main/java/com/example/outsourcing/entity/Menu.java
@@ -1,12 +1,13 @@
 package com.example.outsourcing.entity;
 
+import com.example.outsourcing.eunm.MenuStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 
 @Getter
 @Entity
 @Table(name = "menu")
-public class Menu {
+public class Menu extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -20,6 +21,9 @@ public class Menu {
     @Column(nullable = false)
     private long price;
 
+    @Column(nullable = false)
+    private MenuStatus menuStatus;
+
     @ManyToOne
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
@@ -31,9 +35,21 @@ public class Menu {
     public Menu() {
     }
 
-    public Menu(String name, String description, long price) {
+    public Menu(String name, String description, long price, MenuStatus menuStatus,Store store) {
         this.name = name;
         this.description = description;
         this.price = price;
+        this.menuStatus = menuStatus;
+        this.store = store;
+    }
+
+    public void updateMenu(String name, String description, long price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public void deleteMenu() {
+        this.menuStatus = MenuStatus.DELETED;
     }
 }
