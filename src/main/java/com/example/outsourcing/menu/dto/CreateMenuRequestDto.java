49fa5c2f0dcd087b3b16a54ea1d4442cc1dd1985
Mini9709 <tjdals9709@naver.com>
2024-12-04 16:52:23 +0900

    feat : 메뉴 생성 DTO 추가

---
+++ src/main/java/com/example/outsourcing/menu/dto/CreateMenuRequestDto.java
@@ -0,0 +1,22 @@
+package com.example.outsourcing.menu.dto;
+
+import com.example.outsourcing.entity.Menu;
+import com.example.outsourcing.entity.Store;
+import com.example.outsourcing.eunm.MenuStatus;
+import lombok.Getter;
+
+@Getter
+public class CreateMenuRequestDto {
+    private String menuName;
+    private String menuDescription;
+    private long menuPrice;
+
+    public Menu toEntity(Store store) {
+        return new Menu(
+                this.menuName
+                ,this.menuDescription
+                ,this.menuPrice
+                , MenuStatus.NORMAL
+                ,store);
+    }
+}
