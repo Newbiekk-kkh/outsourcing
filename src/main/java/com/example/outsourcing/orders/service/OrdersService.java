f5b9732b1cc9fbadd57356b216e63bf83a9b908e
Newbiekk <newbiekk1126@gmail.com>
2024-12-05 16:32:51 +0900

    fix : 단일 주문 조회 버그 수정
    주문을 한 사람으로 로그인해서 주문을 조회했을시 권한이 없다고 조회되지 않던 부분 수정

--- src/main/java/com/example/outsourcing/orders/service/OrdersService.java
+++ src/main/java/com/example/outsourcing/orders/service/OrdersService.java
@@ -89,13 +89,12 @@ public class OrdersService {
     public CommonResponseBody<OrdersResponseDto> findOrders(Long storeId, Long ordersId) throws OrdersException {
         // 로그인한 유저가 고객이거나 Owner 일때만 조회가능, 로그인한 유저가 가게 오너이거나 주문한 고객인지 확인하는 코드 필요
         Orders findOrders = ordersRepository.findByIdOrElseThrow(ordersId);
+        if (!findOrders.getMember().getId().equals(sessionUtils.getLoggedInUserId()) && !findOrders.getStore().getMember().getId().equals(sessionUtils.getLoggedInUserId())) {
+            throw new OrdersException(OrdersErrorCode.NOT_ALLOWED_ACCESS);
+        }
 
         if (findOrders.getStore().getId() != storeId) {
             throw new IllegalArgumentException("본 가게의 주문이 아니므로 조회할 수 없습니다.");
-        }
-
-        if (!findOrders.getMember().getId().equals(sessionUtils.getLoggedInUserId()) || !findOrders.getStore().getMember().getId().equals(sessionUtils.getLoggedInUserId())) {
-            throw new OrdersException(OrdersErrorCode.NOT_ALLOWED_ACCESS);
         }
 
         return new CommonResponseBody<>("조회에 성공했습니다.",OrdersResponseDto.toDto(findOrders), HttpStatus.OK);
