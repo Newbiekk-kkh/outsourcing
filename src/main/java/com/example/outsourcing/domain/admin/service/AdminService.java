99b1bc1203475bcc5407b4684bd2a9216d9271c3
dmsdmst <wtgda98@gmail.com>
2024-12-06 16:55:16 +0900

    git pull
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/com/example/outsourcing/domain/admin/service/AdminService.java
+++ src/main/java/com/example/outsourcing/domain/admin/service/AdminService.java
@@ -1,7 +1,19 @@
 package com.example.outsourcing.domain.admin.service;
 
+import com.example.outsourcing.domain.admin.dto.AdminStatsResponseDto;
+import com.example.outsourcing.domain.orders.repository.OrdersRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminService {
+    private final OrdersRepository ordersRepository;
+
+    public AdminStatsResponseDto getDailyStats() {
+//        List<Orders> ordersList= ordersRepository.findByStatus(OrdersStatus.DELIVERED);
+
+
+        return new AdminStatsResponseDto();
+    }
 }
